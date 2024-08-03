646d1acb7b625aabb8f2ec4435617705c71cb089
Fake.User <russellboswell@protonmail.com>
2024-08-02 23:21:18 -0400

    init

---
+++ srvr.js
@@ -0,0 +1,47 @@
+async function getCreds(){
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Auth-Email": `${process.env.X_AUTH_EMAIL}`,
+            "X-Auth-Key": `${process.env.X_AUTH_KEY}`
+        },
+        body: `{
+            "bucket": "destruct-data",
+            "parentAccessKeyId": "${process.env.AWS_ACCESS_KEY_ID}",
+            "permission": "object-read-only",
+            "ttlSeconds": 3600
+        }`
+    };
+
+    let creds = {};
+
+    await fetch(`https://api.cloudflare.com/client/v4/accounts/${process.env.ACCOUNT_ID}/r2/temp-access-credentials`, options)
+    .then(res => res.json())
+    .then(res => {
+        creds = {
+            accessKeyId: res.result.accessKeyId,
+            secretAccessKey: res.result.secretAccessKey,
+            sessionToken: res.result.sessionToken
+        }
+    })
+
+    return creds;
+};
+
+Bun.serve({
+    port: 3000,
+    async fetch(req){
+        const path = new URL(req.url).pathname;
+        switch(true){
+
+            case path === "/creds":
+                let creds = await getCreds();
+                console.log(creds);
+                return Response.json(creds);
+
+            default:
+                return Response("nah mate");
+        };
+    },
+});
